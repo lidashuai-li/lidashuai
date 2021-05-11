703c79eb3627573666a2234de4ec9e0f77fc5436
lidashuai-li <78547426+lidashuai-li@users.noreply.github.com>
2021-05-11 17:25:38 +0800

    2

---
+++ init_all_MD5.sh
@@ -0,0 +1,9 @@
+
+Canary=2021051002
+    init_data_ID=init_data.sh
+    init_data_MD5=d41bed105bf58392de70d710b2d0b058
+    Util_Functions_ID=Util_Functions.sh
+    Util_Functions_MD5=afabfc1669800e2789cf8dc7b8b14bef
+    Cloud_ID=Cloud_Page.zip
+    Cloud_MD5=1d72204733c11ae60ea1cd742625dc6c
+    Cloud_Version=2021051101
